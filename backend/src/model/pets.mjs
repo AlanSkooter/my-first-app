3786fec38ccf34ea6ba69936040744bd2c4208d1
AlanSkooter <alinasozonovich@gmail.com>
2022-05-21 15:13:10 +0300

    add sharp

--- backend/src/model/pets.mjs
+++ backend/src/model/pets.mjs
@@ -1,10 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import formidable from 'formidable';
+import sharp from 'sharp';
 
 export const fetchAllPets = () => {
     let imagesPets = [];
-    const frontendImagesPath = path.resolve(process.cwd(), './frontend/images/');
+    const frontendImagesPath = path.resolve(process.cwd(), './frontend/images/original/');
     const images = fs.readdirSync(frontendImagesPath);
     images.forEach((image) => {
         const ext = image.split('.')[1];
@@ -12,12 +13,18 @@ export const fetchAllPets = () => {
             imagesPets.push(image);
         }
     });
+    imagesPets.forEach((imageName) => {
+        const image = fs.readFileSync(path.resolve(process.cwd(), './frontend/images/original/', imageName));
+        sharp(image)
+        .resize(1000, 600)
+        .toFile(path.resolve(process.cwd(), './frontend/images/', imageName));
+    });
     return imagesPets;
 }
 
 export const upload = () => {
     const form = formidable({
-        uploadDir: `${process.cwd()}/frontend/images`,
+        uploadDir: `${process.cwd()}/frontend/images/original`,
         multiples: true,
         filename: ($, _, {originalFilename}) => `${originalFilename}`,
       });
